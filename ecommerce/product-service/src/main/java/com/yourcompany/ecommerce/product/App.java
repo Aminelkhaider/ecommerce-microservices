671c444f63051a017f98faaa49de61cf425d6814
Amine el khaider <elkhaideramine@gmail.com>
2025-09-21 15:09:41 +0000

    initialize multi-module Spring Boot project

---
+++ ecommerce/product-service/src/main/java/com/yourcompany/ecommerce/product/App.java
@@ -0,0 +1,13 @@
+package com.yourcompany.ecommerce.product;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
